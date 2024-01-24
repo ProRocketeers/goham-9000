9acca021ccc312bb47502984a199c813e3d7f75e
Tr3ch <polak.martin16@gmail.com>
2024-01-24 18:54:53 +0100

    add_getRepo_getRepos_newRepo_delRepo

--- database/database.go
+++ database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fiber_proketo/model"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -8,3 +10,16 @@ import (
 var (
 	DBConn *gorm.DB
 )
+
+func InitDatabase() {
+	var err error
+	DBConn, err = gorm.Open("sqlite3", "repos.db")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("DB Connected")
+
+	DBConn.AutoMigrate(&model.Repository{})
+	fmt.Println("DB Migrated")
+
+}
